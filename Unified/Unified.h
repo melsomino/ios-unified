622b4220c9e5e8db1b492cef145c505ffeda548c
Власов М.Ю <mu.vlasov@tensor.ru>
2016-06-14 09:07:50 +0500

    Project created

---
+++ Unified/Unified.h
@@ -0,0 +1,19 @@
+//
+//  Unified.h
+//  Unified
+//
+//  Created by Власов М.Ю. on 14.06.16.
+//  Copyright © 2016 melsomino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Unified.
+FOUNDATION_EXPORT double UnifiedVersionNumber;
+
+//! Project version string for Unified.
+FOUNDATION_EXPORT const unsigned char UnifiedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Unified/PublicHeader.h>
+
+
